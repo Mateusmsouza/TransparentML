dc213f9b6058a8fea19f7707303f2ffa81c6de50
Mateus Souza <mateus_msouza@outlook.com>
2025-08-18 08:48:03 -0300

    fix(monorepo): app start task

--- apps/backend/db/init_db.py
+++ apps/backend/db/init_db.py
@@ -3,7 +3,4 @@ from pymongo import AsyncMongoClient
 from beanie import Document, Indexed, init_beanie
 
 async def init_db(client: AsyncMongoClient, models: list[Document]):
-    client = AsyncMongoClient(
-        "mongodb://user:pass@host:27017"
-    )
     await init_beanie(database=client.db_name, document_models=models)
